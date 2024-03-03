f67f3a8a97b6e135290026f26d19d63fb21300dd
muazkadan <muaz.kadan@gmail.com>
2024-03-03 09:09:33 +0300

    set scanner options according to user choices

--- app/src/main/java/net/muazkadan/mlkitdocumentscancompose/MainActivity.kt
+++ app/src/main/java/net/muazkadan/mlkitdocumentscancompose/MainActivity.kt
@@ -23,12 +23,15 @@ import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -40,6 +43,7 @@ import androidx.core.text.isDigitsOnly
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
 import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
 import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
+import kotlinx.coroutines.launch
 import net.muazkadan.mlkitdocumentscancompose.ui.theme.DocumentScanComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,6 +58,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val context = LocalContext.current
+                    val scope = rememberCoroutineScope()
+                    val snackbarHostState = remember { SnackbarHostState() }
                     val modes = listOf(
                         stringResource(R.string.full),
                         stringResource(R.string.base), stringResource(R.string.base_with_filter)
@@ -62,20 +68,8 @@ class MainActivity : ComponentActivity() {
                     var selectedOptionText by remember { mutableStateOf(modes[0]) }
                     var enableGalleryImport by remember { mutableStateOf(true) }
                     var pageLimit by remember { mutableIntStateOf(2) }
+                    var scannerMode by remember { mutableIntStateOf(GmsDocumentScannerOptions.SCANNER_MODE_FULL) }
 
-                    val scanner = remember {
-                        val options =
-                            GmsDocumentScannerOptions.Builder() // configure the scanner options
-                                .setGalleryImportAllowed(true)
-                                .setPageLimit(2)
-                                .setResultFormats(
-                                    GmsDocumentScannerOptions.RESULT_FORMAT_JPEG,
-                                    GmsDocumentScannerOptions.RESULT_FORMAT_PDF
-                                )
-                                .setScannerMode(GmsDocumentScannerOptions.SCANNER_MODE_FULL)
-                                .build()
-                        GmsDocumentScanning.getClient(options)
-                    }
                     val scannerLauncher =
                         rememberLauncherForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
                             if (result.resultCode == RESULT_OK) {
@@ -92,7 +86,9 @@ class MainActivity : ComponentActivity() {
                             }
                         }
 
-                    Scaffold { paddingValues ->
+                    Scaffold(
+                        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+                    ) { paddingValues ->
                         Column(
                             modifier = Modifier
                                 .fillMaxSize()
@@ -142,6 +138,12 @@ class MainActivity : ComponentActivity() {
                                                     onClick = {
                                                         selectedOptionText = selectionOption
                                                         expanded = false
+                                                        scannerMode = when (selectionOption) {
+                                                            context.getString(R.string.full) -> GmsDocumentScannerOptions.SCANNER_MODE_FULL
+                                                            context.getString(R.string.base) -> GmsDocumentScannerOptions.SCANNER_MODE_BASE
+                                                            context.getString(R.string.base_with_filter) -> GmsDocumentScannerOptions.SCANNER_MODE_BASE_WITH_FILTER
+                                                            else -> GmsDocumentScannerOptions.SCANNER_MODE_FULL
+                                                        }
                                                     },
                                                     contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
                                                 )
@@ -165,23 +167,40 @@ class MainActivity : ComponentActivity() {
                             ) {
                                 Text(text = stringResource(R.string.set_page_limit_per_scan))
                                 OutlinedTextField(
-                                    value = "2",
+                                    value = "$pageLimit",
                                     onValueChange = {
-                                        if (it.isDigitsOnly()) pageLimit = it.toInt()
+                                        if (it.isNotEmpty() && it.isDigitsOnly()) {
+                                            pageLimit = it.toInt()
+                                        }
                                     },
                                     modifier = Modifier.fillMaxWidth(),
                                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
                                 )
                             }
                             Button(onClick = {
+                                val scannerOptions = GmsDocumentScannerOptions.Builder()
+                                    .setGalleryImportAllowed(enableGalleryImport)
+                                    .setPageLimit(pageLimit)
+                                    .setResultFormats(
+                                        GmsDocumentScannerOptions.RESULT_FORMAT_JPEG,
+                                        GmsDocumentScannerOptions.RESULT_FORMAT_PDF
+                                    )
+                                    .setScannerMode(scannerMode)
+                                    .build()
+                                val scanner = GmsDocumentScanning.getClient(scannerOptions)
                                 scanner.getStartScanIntent(context as Activity)
                                     .addOnSuccessListener { intentSender ->
                                         scannerLauncher.launch(
                                             IntentSenderRequest.Builder(intentSender).build()
                                         )
                                     }.addOnFailureListener {
-                                    // handle failure
-                                }
+                                        scope.launch {
+                                            snackbarHostState.showSnackbar(
+                                                it.localizedMessage
+                                                    ?: getString(R.string.something_went_wrong)
+                                            )
+                                        }
+                                    }
                             }) {
                                 Text(stringResource(R.string.scan))
                             }
